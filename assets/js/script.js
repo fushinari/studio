079d1d1325317fcf71325673bc783d6a8f06f7f2
Mufeed Ali <fushinari@protonmail.com>
2021-02-01 19:25:37 +0530

    Transparent Navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,106 +1,147 @@
-// Preloader js    
-$(window).on('load', function () {
-	$('.preloader').fadeOut(100);
+// Preloader js
+$(window).on("load", function () {
+  $(".preloader").fadeOut(100);
 });
 
 $(document).ready(function () {
-	'use strict';
+  "use strict";
 
-	// Shuffle js filter and masonry
-	var containerEl = document.querySelector('.shuffle-wrapper');
-	if (containerEl) {
-		var Shuffle = window.Shuffle;
-		var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
-			itemSelector: '.shuffle-item',
-			buffer: 1
-		});
+  // Shuffle js filter and masonry
+  var containerEl = document.querySelector(".shuffle-wrapper");
+  if (containerEl) {
+    var Shuffle = window.Shuffle;
+    var myShuffle = new Shuffle(document.querySelector(".shuffle-wrapper"), {
+      itemSelector: ".shuffle-item",
+      buffer: 1,
+    });
 
-		jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
-			var input = evt.currentTarget;
-			if (input.checked) {
-				myShuffle.filter(input.value);
-			}
-		});
-	}
+    jQuery('input[name="shuffle-filter"]').on("change", function (evt) {
+      var input = evt.currentTarget;
+      if (input.checked) {
+        myShuffle.filter(input.value);
+      }
+    });
+  }
 
-	$('.portfolio-single-slider').slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 2000
+  $(".portfolio-single-slider").slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 
-	});
+  $(".clients-logo").slick({
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 
-	$('.clients-logo').slick({
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 2000
-	});
+  $(".testimonial-slider").slick({
+    slidesToShow: 1,
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
 
-	$('.testimonial-slider').slick({
-		slidesToShow: 1,
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 2000
-	});
+  $(".home-slider").slick({
+    slidesToShow: 1,
+    infinite: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
 
-	$('.home-slider').slick({
-		slidesToShow: 1,
-		infinite: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 3000
-	});
+  // Magnific Popup Image
+  $(".portfolio-popup").magnificPopup({
+    type: "image",
+    removalDelay: 160, //delay removal by X to allow out-animation
+    callbacks: {
+      beforeOpen: function () {
+        // just a hack that adds mfp-anim class to markup
+        this.st.image.markup = this.st.image.markup.replace(
+          "mfp-figure",
+          "mfp-figure mfp-with-anim"
+        );
+        this.st.mainClass = this.st.el.attr("data-effect");
+      },
+    },
+    closeOnContentClick: true,
+    midClick: true,
+    fixedContentPos: true,
+    fixedBgPos: true,
+  });
 
+  //  Count Up
+  function counter() {
+    var oTop;
+    if ($(".count").length !== 0) {
+      oTop = $(".count").offset().top - window.innerHeight;
+    }
+    if ($(window).scrollTop() > oTop) {
+      $(".count").each(function () {
+        var $this = $(this),
+          countTo = $this.attr("data-count");
+        $({
+          countNum: $this.text(),
+        }).animate(
+          {
+            countNum: countTo,
+          },
+          {
+            duration: 1000,
+            easing: "swing",
+            step: function () {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function () {
+              $this.text(this.countNum);
+            },
+          }
+        );
+      });
+    }
+  }
+  $(window).on("scroll", function () {
+    counter();
+  });
+});
 
-	// Magnific Popup Image
-	$('.portfolio-popup').magnificPopup({
-		type: 'image',
-		removalDelay: 160, //delay removal by X to allow out-animation
-		callbacks: {
-			beforeOpen: function () {
-				// just a hack that adds mfp-anim class to markup
-				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
-				this.st.mainClass = this.st.el.attr('data-effect');
-			}
-		},
-		closeOnContentClick: true,
-		midClick: true,
-		fixedContentPos: true,
-		fixedBgPos: true
-	});
+// The debounce function receives our function as a parameter
+const debounce = (fn) => {
+  // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+  let frame;
 
-	//  Count Up
-	function counter() {
-		var oTop;
-		if ($('.count').length !== 0) {
-			oTop = $('.count').offset().top - window.innerHeight;
-		}
-		if ($(window).scrollTop() > oTop) {
-			$('.count').each(function () {
-				var $this = $(this),
-					countTo = $this.attr('data-count');
-				$({
-					countNum: $this.text()
-				}).animate({
-					countNum: countTo
-				}, {
-					duration: 1000,
-					easing: 'swing',
-					step: function () {
-						$this.text(Math.floor(this.countNum));
-					},
-					complete: function () {
-						$this.text(this.countNum);
-					}
-				});
-			});
-		}
-	}
-	$(window).on('scroll', function () {
-		counter();
-	});
+  // The debounce function returns a new function that can receive a variable number of arguments
+  return (...params) => {
+    // If the frame variable has been defined, clear it now, and queue for next frame
+    if (frame) {
+      cancelAnimationFrame(frame);
+    }
 
+    // Queue our function call for the next frame
+    frame = requestAnimationFrame(() => {
+      // Call our function and pass any params we received
+      fn(...params);
+    });
+  };
+};
+
+// Reads out the scroll position and stores it in the data attribute
+// so we can use it in our stylesheets
+const storeScroll = () => {
+  document.documentElement.dataset.scroll = window.scrollY;
+};
+
+// Listen for new scroll events, here we debounce our `storeScroll` function
+document.addEventListener("scroll", debounce(storeScroll), { passive: true });
+
+// Update scroll position for first time
+storeScroll();
+
+$(document).ready(function () {
+  $(".navbar-dark > button").on("click", function () {
+    $(".navbar-dark").toggleClass("color-changed");
+  });
 });
