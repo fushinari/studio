1674e18f93a14f99e22618e3e60a4b4b2fda7a2c
Mufeed Ali <fushinari@protonmail.com>
2021-02-01 23:24:25 +0530

    Give "Studio" an identity
    - Much improved typography with Lato and better weights
    - Upper case nav bar buttons
    - Dots in home slider (banner)

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,8 +49,11 @@ $(document).ready(function () {
     slidesToShow: 1,
     infinite: true,
     arrows: false,
+    dots: true,
     autoplay: true,
     autoplaySpeed: 3000,
+    fade: true,
+    fadespeed: 2000,
   });
 
   // Magnific Popup Image
@@ -108,36 +111,25 @@ $(document).ready(function () {
   });
 });
 
-// The debounce function receives our function as a parameter
 const debounce = (fn) => {
-  // This holds the requestAnimationFrame reference, so we can cancel it if we wish
   let frame;
 
-  // The debounce function returns a new function that can receive a variable number of arguments
   return (...params) => {
-    // If the frame variable has been defined, clear it now, and queue for next frame
     if (frame) {
       cancelAnimationFrame(frame);
     }
 
-    // Queue our function call for the next frame
     frame = requestAnimationFrame(() => {
-      // Call our function and pass any params we received
       fn(...params);
     });
   };
 };
 
-// Reads out the scroll position and stores it in the data attribute
-// so we can use it in our stylesheets
 const storeScroll = () => {
   document.documentElement.dataset.scroll = window.scrollY;
 };
 
-// Listen for new scroll events, here we debounce our `storeScroll` function
 document.addEventListener("scroll", debounce(storeScroll), { passive: true });
-
-// Update scroll position for first time
 storeScroll();
 
 $(document).ready(function () {
